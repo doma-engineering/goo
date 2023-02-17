ebe0a5ef7d98a3fd3768d5f6f00865a79388bb5e
cognivore <66186054+cognivore@users.noreply.github.com>
2023-02-17 11:41:50 +0000

    Problem: No reproducible dependencies
    This is a problem because even though the Makefile chekcs for
    reproducibility.
    
    Solution: add dependency management using Nix flakes.

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
+    };
+
+    outputs = {self, nixpkgs}:
+        let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in {
+            defaultPackage.x86_64-linux = pkgs.hello;
+
+            devShell.x86_64-linux =
+                pkgs.mkShell {
+                    buildInputs = [
+                        pkgs.erlang
+                    ];
+                };
+        };
+}
